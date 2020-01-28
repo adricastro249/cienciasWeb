4667e5bd0a6068dcee15275420c59715eb990a4c
adricastro249 <adricastro249@gmail.com>
2020-01-28 18:20:04 -0300

    martes6pm

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,10 @@
 // ********** boton SECCIONES
-$("#masSecc").click(function() {
+$("#masSecc").click(function () {
     console.log("dia 2");
-    
+
     $("#seccion3").show();
     $("#seccion4").show();
-    })
+})
 
 //  ******** agrega titulo a SECCIONES- falta traer los datos del jsonGral, pero no estan los datos incluidos 
 $("#seccion4 .tituloCategoria").append("OTRO NOMBRE")
@@ -12,10 +12,63 @@ $("#seccion4 .tituloCategoria").append("OTRO NOMBRE")
 // ********* AGENDA HOY
 
 // trae el titutlo
-$( window ).on( "load", function() {
-    console.log( this.event );
-    console.log( this.event.target.title );
+$(window).on("load", function () {
+    console.log(this.event);
+    console.log(this.event.target.title);
     let titulo = this.event.target.title;
     $("#tituloPag").append(`<h1 class="col-3 text-right"> ${titulo} </h1> 
     <img src="./images/linea-ROSA.png" alt="lineaRosa" class="col-3">`)
 });
+
+
+
+
+// ********** boton Paginación Comprar
+$("#segundoBloque").hide();
+
+$("#masBloq2Rev").click(function () {
+    $("#segundoBloque").show();
+    $("#primerBloque").hide();
+})
+
+$("#masBloqSigComprar").click(function () {
+    $("#segundoBloque").show();
+    $("#primerBloque").hide();
+})
+
+$("#masBloq1Rev").click(function () {
+    $("#segundoBloque").hide();
+    $("#primerBloque").show();
+})
+
+$("#masBloqPrevCompra").click(function () {
+    $("#segundoBloque").hide();
+    $("#primerBloque").show();
+})
+
+
+// ********** boton Paginación Revistas
+$("#segundosEventos").hide();
+
+$("#masBloq2Eventos").click(function () {
+    $("#segundosEventos").show();
+    $("#primerosEventos").hide();
+})
+
+$("#masSigEventos").click(function () {
+    $("#segundosEventos").show();
+    $("#primerosEventos").hide();
+})
+
+$("#masBloq1Eventos").click(function () {
+    $("#segundosEventos").hide();
+    $("#primerosEventos").show();
+})
+
+$("#masPrevEventos").click(function () {
+    $("#segundosEventos").hide();
+    $("#primerosEventos").show();
+})
+
+
+// ********** Formulario
