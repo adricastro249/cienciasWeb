c633086a54aa0c52933c183d307bf8072b831534
adricastro249 <adricastro249@gmail.com>
2020-01-29 17:36:57 -0300

    revComprar

---
+++ public/js/comprar.js
@@ -0,0 +1,22 @@
+// ********** boton Paginación Comprar
+$("#segundoBloque").hide();
+
+$("#masBloq2Rev").click(function () {
+    $("#segundoBloque").show();
+    $("#primerBloque").hide();
+})
+
+$("#masBloqSigComprar").click(function () {
+    $("#segundoBloque").show();
+    $("#primerBloque").hide();
+})
+
+$("#masBloq1Rev").click(function () {
+    $("#segundoBloque").hide();
+    $("#primerBloque").show();
+})
+
+$("#masBloqPrevCompra").click(function () {
+    $("#segundoBloque").hide();
+    $("#primerBloque").show();
+})
