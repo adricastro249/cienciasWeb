e948bb2f15ba59f2006dd826558e18532ae4a273
adricastro249 <adricastro249@gmail.com>
2020-01-31 11:33:54 -0300

    carrito

--- public/js/comprar.js
+++ public/js/comprar.js
@@ -1,6 +1,7 @@
 // ********** boton Paginación Comprar
 $("#segundoBloque").hide();
 
+
 $("#masBloq2Rev").click(function () {
     $("#segundoBloque").show();
     $("#primerBloque").hide();
@@ -21,24 +22,63 @@ $("#masBloqPrevCompra").click(function () {
     $("#primerBloque").show();
 })
 
-
-/*/--Function Agregar al Carrito/*/
-
+/*/--Esconder carrito y texto revistas/*/
 var carrito = [];
+var misRevistas = [];
+
+$("#textRevistas").show();
+$("#txtCarrito").show();
+$("#btncomprarCarrito").hide();
+
+
 function agregCarrito () {
 	console.log(event);
-	
 	let click = event.toElement.name;
 	console.log(click);
 	carrito.push(click)
-    console.log("mis eventos son..." + carrito);
-    
+	console.log("mis revistas son..." + carrito);
+	contCarrito()   
 }
 
 console.log(carrito);
 
+function contCarrito() {
+// Revistas en carrito
+if (carrito.includes("comprarIA" || "comprarCN" || "comprarMUN")) {
+	console.log("incluye comprarIA");
+	$("#btncomprarCarrito").show();
+	$("#txtCarrito").hide();	
+} else if (!carrito.includes("comprarIA" || "comprarCN" || "comprarMUN")){
+	console.log("No incluye comprarIA");
+	$("#btncomprarCarrito").hide();
+	$("#txtCarrito").show();
+}
+}
 
 
+function comprar () {
+	console.log(event);
+	let click = event.toElement.name;
+	console.log(click);
+	misRevistas.push(click)
+	console.log("mis revistas son..." + misRevistas); 
+}
+
+function contRevistas() {
+	// Revistas en carrito
+	if (misRevistas.includes("carrito")) {
+		console.log("incluye contenido carrito");
+		$("#textRevistas").hide();	
+	} else if (!misRevistas.includes("carrito")){
+		console.log("No incluye contenido carrito");
+		$("#textRevistas").show();
+	}
+	}
+
+
+
+
+/*/--Function Agregar al Carrito/*/
 /* function desinscribirse () {
 	console.log(event);
 	let click = event.toElement.name;
