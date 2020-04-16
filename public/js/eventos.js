ec0521534b75b6d2c11e73adf1d88a6eadb9caea
adricastro249 <adricastro249@gmail.com>
2020-04-16 17:16:23 -0300

    secciones

--- public/js/eventos.js
+++ public/js/eventos.js
@@ -1,12 +1,12 @@
 // ********* AGENDA HOY
 
 // evento 1
-document.getElementById("tituloEvent1").innerHTML= data.eventos[0].titulo;
+/* document.getElementById("tituloEvent1").innerHTML= data.eventos[0].titulo;
 document.getElementById("notaEvent1").innerHTML= data.eventos[0].texto;
 document.getElementById("mesEvent1").innerHTML= data.eventos[0].month;
 document.getElementById("diaEvent1").innerHTML= data.eventos[0].day;
-document.getElementById("horaEvent1").innerHTML= data.eventos[0].time;
-// document.getElementById("nota1").getElementsByTagName("tbody")[0].innerHTML=resultado
+document.getElementById("horaEvent1").innerHTML= data.eventos[0].time; */
+
 
 // evento 2
 
@@ -16,20 +16,7 @@ document.getElementById("mesEvent2").innerHTML= data.eventos[1].month;
 document.getElementById("diaEvent2").innerHTML= data.eventos[1].day;
 document.getElementById("horaEvent2").innerHTML= data.eventos[1].time;
 
-// evento 3
 
-document.getElementById("tituloEvent3").innerHTML= data.eventos[1].titulo;
-document.getElementById("notaEvent3").innerHTML= data.eventos[1].texto;
-document.getElementById("mesEvent3").innerHTML= data.eventos[1].month;
-document.getElementById("diaEvent3").innerHTML= data.eventos[1].day;
-document.getElementById("horaEvent3").innerHTML= data.eventos[1].time;
-
-// evento 1
-document.getElementById("tituloEvent4").innerHTML= data.eventos[0].titulo;
-document.getElementById("notaEvent4").innerHTML= data.eventos[0].texto;
-document.getElementById("mesEvent4").innerHTML= data.eventos[0].month;
-document.getElementById("diaEvent4").innerHTML= data.eventos[0].day;
-document.getElementById("horaEvent4").innerHTML= data.eventos[0].time;
 
 // ******* Inscribirme / desincribirme
 const misEventos = [];
@@ -66,33 +53,6 @@ if (!misEventos.includes("evento2")) {
 }
 
 
-// evento3
-if (!misEventos.includes("evento3")) {
-	console.log("No incluye el evento3");
-	
-	$("#botonQuitaE3").hide();
-	$("#botonAgregaE3").show();
-	
-} else if (misEventos.includes("evento3")){
-	console.log(misEventos);
-	console.log("incluye el evento3");
-	$("#botonAgregaE3").hide();
-	$("#botonQuitaE3").show();
-}
-
-// evento4
-if (!misEventos.includes("evento4")) {
-	console.log("No incluye el evento4");
-	
-	$("#botonQuitaE4").hide();
-	$("#botonAgregaE4").show();
-	
-} else if (misEventos.includes("evento4")){
-	console.log(misEventos);
-	console.log("incluye el evento4");
-	$("#botonAgregaE4").hide();
-	$("#botonQuitaE4").show();
-}
 
 }
 
---
+++ public/js/secciones.js
@@ -0,0 +1,134 @@
+var nodeSecc = document.getElementById("superior");
+
+data.secciones.forEach(secc => {
+    let nodeSecc0 = document.createElement('DIV')
+    nodeSecc0.classList.add("mt-5")
+    nodeSecc0.classList.add("p-2")
+    nodeSecc0.classList.add("flex-grow-1")
+    nodeSecc0.classList.add("bd-highlight")
+
+    let nodeSecc1 = document.createElement('DIV')
+    nodeSecc1.classList.add("d-flex")
+    nodeSecc1.classList.add("justify-content-between")
+
+    let nodeSecc2 = document.createElement('H2')
+    nodeSecc2.classList.add("tituloCategoria")
+   nodeSecc2a = document.createTextNode(secc.nombreSecc) 
+
+    let nodeSecc3 = document.createElement('H5')
+    nodeSecc3.classList.add("etiquetas")
+ nodeSecc3a = document.createTextNode(secc.etiquetas) 
+
+    let nodeSecc4 = document.createElement('BUTTON')
+    nodeSecc4.classList.add("btnNotasSecc")
+    nodeSecc4.classList.add("pl-5")
+    nodeSecc4.classList.add("pr-5")
+    nodeSecc4.setAttribute('type', "button");
+    nodeSecc4.setAttribute('id', secc.id);
+    nodeSecc4.setAttribute('value', secc.codSecc);
+   nodeSecc4a = document.createTextNode("Ver más notas")
+
+    nodeSecc5 = document.createElement('HR');
+    nodeSecc5.classList.add("divSecciones");
+
+
+    nodeSecc.appendChild(nodeSecc0)
+    nodeSecc0.appendChild(nodeSecc1)
+    nodeSecc1.appendChild(nodeSecc2)
+    nodeSecc2.appendChild(nodeSecc2a)
+    nodeSecc1.appendChild(nodeSecc3)
+    nodeSecc3.appendChild(nodeSecc3a)
+    nodeSecc1.appendChild(nodeSecc4)
+    nodeSecc4.appendChild(nodeSecc4a)
+    nodeSecc0.appendChild(nodeSecc5)
+
+
+
+
+
+    let nodeNota = document.createElement('DIV')
+    nodeNota.classList.add("row")
+
+    let nodeNota1 = document.createElement('DIV')
+    nodeNota1.classList.add("secCards")
+
+    let nodeNota2 = document.createElement('DIV')
+    nodeNota2.classList.add("d-flex")
+    nodeNota2.classList.add("flex-wrap")
+    nodeNota2.classList.add("justify-content-around")
+
+    secc.notas.forEach(val => {
+
+    
+
+        let nodeNota3 = document.createElement('DIV')
+        nodeNota3.classList.add("card")
+        nodeNota3.classList.add("border-0")
+        nodeNota3.classList.add("mb-5")
+
+        let nodeNota4 = document.createElement('A')
+        nodeNota4.setAttribute('href', val.href);
+
+        let nodeNotaImg = document.createElement('IMG')
+        nodeNotaImg.classList.add("card-img-top")
+        nodeNotaImg.setAttribute('src', val.imagen);
+
+        let nodeNota5 = document.createElement('DIV')
+        nodeNota5.classList.add("mt-2")
+        nodeNota5.classList.add("d-flex")
+        nodeNota5.classList.add("justify-content-between")
+
+        let nodeNota6 = document.createElement('H5')
+        nodeNota6.classList.add("text-left")
+        nodeNota6.classList.add("autor")
+        nodeNota6 = document.createTextNode(val.autor);
+
+
+        let nodeNota7 = document.createElement('DIV')
+        nodeNota7.classList.add("imgFlecha")
+
+        let nodeNota8 = document.createElement('IMG')
+        nodeNota8.classList.add("mt-2")
+        nodeNota8.setAttribute('src', "./images/Home/flecha_notas.png");
+        nodeNota8.setAttribute("alt", "flecha");
+
+        let nodeNota9 = document.createElement('A')
+        nodeNota9.classList.add("text-dark")
+        nodeNota9.classList.add("titNotaMV")
+        nodeNota9.classList.add("text-justify")
+        nodeNota9.setAttribute('href', val.href);
+
+        let nodeTit = document.createElement('STRONG')
+        nodeTit = document.createTextNode(val.titulo);
+
+
+        nodeSecc.appendChild(nodeNota)
+        nodeNota.appendChild(nodeNota1)
+        nodeNota1.appendChild(nodeNota2)
+        nodeNota2.appendChild(nodeNota3)
+        nodeNota3.appendChild(nodeNota4)
+        nodeNota4.appendChild(nodeNotaImg)
+        nodeNota3.appendChild(nodeNota5)
+        nodeNota5.appendChild(nodeNota6)
+        nodeNota5.appendChild(nodeNota7)
+        nodeNota7.appendChild(nodeNota8)
+        nodeNota3.appendChild(nodeNota9)
+        nodeNota9.appendChild(nodeTit)
+
+
+    }) //Cierra forEach de las categorias y marcas de los filtros
+
+
+
+var botonNotasSecc = nodeSecc4
+    console.log(botonNotasSecc);
+    
+
+botonNotasSecc.addEventListener("click", function (e) {
+console.log("prueba2");
+console.log(botonNotasSecc.id);
+  
+})
+
+})
+
